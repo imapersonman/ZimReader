9ee5285654bec48c011dc0db65ba05f9d8da542a
Koissi Adjorlolo <imapersonman@koissis-macbook-air.local>
2016-11-07 14:01:47 -0800

    Initial Commit

---
+++ ZimReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZimReader
+//
+//  Created by Koissi Adjorlolo on 11/7/16.
+//  Copyright © 2016 Koissi Adjorlolo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ZimReader/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ZimReader
+//
+//  Created by Koissi Adjorlolo on 11/7/16.
+//  Copyright © 2016 Koissi Adjorlolo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
